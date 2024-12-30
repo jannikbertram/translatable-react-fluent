32d927c381b4e5a7c78445b6ee71aa9f83ab3171
Jannik Bertram <jb@allgravy.com>
2024-12-30 17:32:16 +0100

    Initial commit

---
+++ config/languages/resources/en.ts
@@ -0,0 +1,12 @@
+import { FluentResource } from "@fluent/bundle";
+
+const translations = new FluentResource(`
+cancel = Cancel
+save = Save
+submit = Submit
+confirm = Confirm
+delete = Delete
+add = Add
+`);
+
+export default translations;
